54f151ef8db22b122975c7d4cd4b3bc79010a029
Gekif <dzulfikar.maulana@gmail.com>
2020-02-04 15:13:41 +0700

    1. Object Oriented Programming: 6. Public and private visibility -Fixed LastName

--- test/Customer.php
+++ test/Customer.php
@@ -5,16 +5,18 @@ class Customer
     private $id;
     private $firstName;
     private $surnName;
+    private $lastName;
     private $email;
 
 
     public function __construct(
-        $id, $firstName, $surnName, $email
+        $id, $firstName, $surnName, $lastName,$email
     )
     {
         $this->id = $id;
         $this->firstName = $firstName;
         $this->surnName = $surnName;
+        $this->lastName = $lastName;
         $this->email = $email;
     }
 
